1b022d1de5652475159ca830e0255ee673d46831
JaramilloCris <jaramillo.cristobal.a@gmail.com>
2022-12-04 16:35:58 -0300

    update directory

---
+++ src/pages/proyects/components/ProyectCard.tsx
@@ -0,0 +1,16 @@
+import Proyect from "../interfaces";
+
+export default function ProyectCard(props: { proyect: Proyect }) {
+  return (
+    <div className="container align-items-center mb-3">
+      <div className="d-flex justify-content-center">
+        <div className="w-75">
+          <h3>{props.proyect.title}</h3>
+          <span>{props.proyect.subTitle}</span>
+          <p>{props.proyect.info}</p>
+          <hr className="mb-3"/>
+        </div>
+      </div>
+    </div>
+  );
+}
